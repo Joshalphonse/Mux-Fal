a1dc9dc42b12167be5f8ccd5d1ff646b13f8d937
joshalphonse <joshalphonse@gmail.com>
2025-09-26 12:35:57 -0700

    created new layout

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    MUX_PLAYBACK_ID: process.env.MUX_PLAYBACK_ID,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.mux.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   /* config options here */
 };
 
